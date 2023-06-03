c3ff81cda33448f4c70f40a717c7dd4ab0cd83c1
adtet <adiyatma.padang@gmail.com>
2023-06-03 22:33:48 +0700

    adit commit first commit

---
+++ src/main/java/com/mycompany/registrasi_asrama/Registrasi_asrama.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrasi_asrama;
+
+import Login.LoginActivity;
+
+/**
+ *
+ * @author adt_et
+ */
+public class Registrasi_asrama {
+
+    public static void main(String[] args) {
+        LoginActivity login = new LoginActivity();
+        login.setVisible(true);
+    }
+}
---
+++ src/main/java/configs/configs.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package configs;
+
+/**
+ *
+ * @author adt_et
+ */
+public class configs {
+    
+    public static final String dbName = "dbAsrama";
+    
+    public static final String dbUrl = "jdbc:mysql://localhost:3306/"+dbName ;
+    
+    public static final String dbUsername = "ayaka";
+    
+    public static final String dbPassword = "ayakaPa$$word2781";
+    
+}
